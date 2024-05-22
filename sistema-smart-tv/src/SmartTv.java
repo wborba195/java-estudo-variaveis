9efb6faa1c8a99e432e0d16aba82f24eec45b80a
wborba195 <willianborbadasilva@yahoo.com.br>
2024-05-21 22:04:38 -0300

    operadores

--- sistema-smart-tv/src/SmartTv.java
+++ sistema-smart-tv/src/SmartTv.java
@@ -2,4 +2,36 @@ public class SmartTv {
     boolean ligada=false;
     int canal=1;
     int volume = 25;
+
+    public void ligar(){
+        ligada = true;
+    }
+
+    public void desligar(){
+        ligada = false;
+    }
+
+    public void aumentarVol(){
+        volume++;
+        System.out.println("Aumentando Volume para: "+volume);
+    }
+
+    public void diminuirVol(){
+        volume--;
+        System.out.println("Diminuindo Volume para: "+volume);
+    }
+
+    public void mudarCanal(int novoCanal){
+        canal = novoCanal;
+    }
+
+    public void aumentarCanal(){
+        canal++;
+    }
+
+    public void diminuirrCanal(){
+        canal--;
+    }
+
+
 }
--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -1,10 +1,35 @@
 public class Usuario {
+    
     public static void main(String[] args) throws Exception {
-        
+
         SmartTv smartTv = new SmartTv();
 
-        System.out.println("TV Ligada? " + smartTv.ligada);
-        System.out.println("Canal Atual? " + smartTv.canal);
-        System.out.println("Volume Atual? " + smartTv.volume);
+        smartTv.diminuirVol();
+        smartTv.diminuirVol();
+        smartTv.aumentarVol();
+
+        smartTv.mudarCanal(5);
+
+        System.out.println(smartTv.canal);
+
+        smartTv.aumentarCanal();
+        System.out.println(smartTv.canal);
+
+        smartTv.diminuirrCanal();
+        System.out.println(smartTv.canal);
+
+        System.out.println(smartTv.volume);
+
+        System.out.println("TV Ligada? "+smartTv.ligada);
+        System.out.println("Canal Atual: "+smartTv.canal);
+        System.out.println("Volume Atual: "+smartTv.volume);
+    
+        smartTv.ligar();
+        System.out.println("TV Ligada? "+smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("TV Ligada? "+smartTv.ligada);
+    
+    
     }
 }
